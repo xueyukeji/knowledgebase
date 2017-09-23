f1de9178e714f055be458c51b96bd86d5408a069
XueShan <mountainpeng@foxmail.com>
2017-09-23 11:38:48 +0800

    test

--- src/components/test.js
+++ src/components/test.js
@@ -2,17 +2,24 @@ import React, {Component} from 'react'
 import {inject, observer} from 'mobx-react'
 
 @inject(stores => {
+    let {username, setUserName} = stores.test
     return {
-        username: stores.test.username
+        username,
+        setUserName
     }
 })
 @observer
 export default class Test extends Component {
+    handleBtnClick = () => {
+        this.props.setUserName(`name: ${Math.random()}`)
+    }
+
     render() {
         let {username} = this.props
         return (
             <div>
                 {username}
+                <button onClick={this.handleBtnClick}>click me</button>
             </div>
         )
     }
