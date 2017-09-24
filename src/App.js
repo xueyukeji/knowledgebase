91eb8b48df85767fc8b9d2ad5e55f7039f0bf150
XueShan <mountainpeng@foxmail.com>
2017-09-24 12:46:57 +0800

    div router component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import './style/app.scss';
 import {Provider} from 'mobx-react';
 import store from './store';
-import Test from './components/test'
-import Test2 from './components/test2'
-import Nav from './components/nav'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import AppRouter from './router'
 
 class App extends Component {
     render() {
         return (
             <Provider {...store}>
-                <Router>
-                    <div>
-                        <Nav />
-                        <Route exact path="/test1" component={Test}/>
-                        <Route path="/test2" component={Test2}/>
-                    </div>
-                </Router>
+                <AppRouter />
             </Provider>
         );
     }
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Test from '../components/test'
+import Test2 from '../components/test2'
+import Nav from '../components/nav'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+export default class AppRouter extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="wrap">
+                    <Nav />
+                    <Route exact path="/test1" component={Test}/>
+                    <Route path="/test2" component={Test2}/>
+                </div>
+            </Router>
+        )
+    }
+}
