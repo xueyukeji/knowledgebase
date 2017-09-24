f42d78ce48345f666861b26f20b21a77b7feb21f
nieying <1411008062@qq.com>
2017-09-24 10:05:31 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
-import App from './app';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
