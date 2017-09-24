92820f63a719274e1d5cb1ac4bf850fe926f1500
XueShan <mountainpeng@foxmail.com>
2017-09-24 11:30:50 +0800

    add element ui

--- src/components/test2.js
+++ src/components/test2.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import {Button} from 'element-react'
 
 export default class Test2 extends Component {
     render() {
         return (
             <div>
                 test2
+                <Button>button</Button>
             </div>
         )
     }
