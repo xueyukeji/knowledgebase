cff96d7415a125c568720571b1ca62884794e3d8
XueShan <mountainpeng@foxmail.com>
2017-10-07 23:27:50 +0800

    reset element color

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import {Provider} from 'mobx-react';
 import store from './store';
-import AppRouter from './router'
-import 'element-theme-default/lib/index.css'
+import AppRouter from './router';
 
 class App extends Component {
     render() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'element-theme-default/lib/index.css';
 import './assets/css/index.scss';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
