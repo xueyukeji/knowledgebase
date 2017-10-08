3d0484bd4a84b64c47401f2673539321a07af92e
XueShan <mountainpeng@foxmail.com>
2017-10-08 17:19:15 +0800

    add coockie

--- src/store/user.js
+++ src/store/user.js
@@ -3,12 +3,22 @@ import {createFetch} from './fetch-creator';
 
 class Store {
     @observable userInfo = {};
+    @observable userFile = [];
 
     @action getUserInfo = () => {
         return createFetch({
             url: 'apps/user'
         }).then(data => {
             this.userInfo = data;
+        });
+    }
+    @action getUserFile = () => {
+        return createFetch({
+            url: 'file',
+            method: 'get',
+            params: {
+                fc: 'personal'
+            }
         });
     }
 }
