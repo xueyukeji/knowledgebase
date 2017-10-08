e50800ae2da91be2603fba053008d95811b8830a
soakit <455288262@qq.com>
2017-10-08 20:03:27 +0800

    remove favicon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'element-theme-default/lib/index.css';
 import './assets/css/index.scss';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
