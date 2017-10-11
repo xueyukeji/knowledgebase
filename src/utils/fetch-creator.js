515609ec3ad696dfca451d2242d3dfcc85fec1c3
soakit <455288262@qq.com>
2017-10-11 22:44:09 +0800

    update

--- src/utils/fetch-creator.js
+++ src/utils/fetch-creator.js
@@ -1,5 +1,5 @@
 import 'isomorphic-fetch';
-// import Cookies from 'js-cookie';
+import Cookies from 'js-cookie';
 
 const URL_PREFIX = process.env.NODE_ENV === 'development' ? '/apps/' : '/apps/';
 
@@ -15,8 +15,8 @@ function createQuery(method, body) {
     const query = {
         'method': method,
         'headers': {
-            // 'ct': Cookies.get('ct'),
-            'ct': 'weg9gpplvxkyce9f1c8d9b172e4747f025617dce336beea158c38ce0f5c492a69b7d87df65770150767779579000000000000000000',
+            'ct': Cookies.get('ct'),
+            // 'ct': '9pvuruqp645e6324197d8c65400db5a8d49a8571d3f947a320f47fc7d4d6bb0bd26dc93f1ca20150766968279000000000000000000',
             'cv': '3.5.0',
             'Accept': 'application/json',
             'Content-type': 'application/json;charset=UTF-8',
